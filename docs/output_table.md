0dffc559be2d522db58bbf86a835a357ab31c753
Chanakya Pallapolu <chanakya@svtrobotics.com>
2021-12-06 09:50:31 -0500

    added 2 parts of puzzle 06

--- docs/output_table.md
+++ docs/output_table.md
@@ -1,8 +1,8 @@
 <table>
 <thead>
-<tr><th>#  </th><th>Part 1  </th><th>Part 1 Time  </th><th>Part 2  </th><th>Part 2 Time  </th><th>Tests  </th><th>Tests Time  </th></tr>
+<tr><th>#  </th><th>Part 1  </th><th>Part 1 Time  </th><th>Part 2       </th><th>Part 2 Time  </th><th>Tests  </th><th>Tests Time  </th></tr>
 </thead>
 <tbody>
-<tr><td>05 </td><td>5294    </td><td>181.420 ms   </td><td>21698   </td><td>109.544 ms   </td><td>2      </td><td>280.536 ms  </td></tr>
+<tr><td>06 </td><td>360610  </td><td>0.215 ms     </td><td>1631629590423</td><td>0.403 ms     </td><td>2      </td><td>1.008 ms    </td></tr>
 </tbody>
 </table>
---
+++ src/years/2021/06/solution.py
@@ -0,0 +1,56 @@
+
+
+from typing import Counter
+
+from aocpuzzle import AoCPuzzle
+
+
+class Puzzle06(AoCPuzzle):
+    def common(self, input_data: str) -> None:
+        # for part1 list would have sufficed but as the days increase the arrray size will get
+        # out of hand and will slow the process. That's why we need dictionary for easy access.
+        self.fishes = Counter(map(int, input_data.split(',')))
+
+    def reproduce(self, days: int, fishes: dict[int, int]) -> int:
+        for _ in range(days):
+            state_after_day = {
+                internal_timer - 1: count
+                for internal_timer, count in fishes.items()
+            }
+
+            # removing fishes that reproduced
+            state_after_day.pop(-1, None)
+
+            # if we have any fishes with 0 timer left,
+            # we add a new fishes equal to the number of fishes with 0 timer
+            # then we reset the timer of these fishes to 6
+            if 0 in fishes:
+                state_after_day[8] = fishes[0]
+                state_after_day[6] = fishes[0] + state_after_day.get(6, 0)
+
+            fishes = state_after_day
+
+        return sum(fishes.values())
+
+    def part1(self) -> int:
+        return self.reproduce(80, self.fishes)
+
+    def part2(self) -> int:
+        return self.reproduce(256, self.fishes)
+
+    def test_cases(self, input_data: str) -> int:
+        tests = [
+            '3,4,3,1,2',
+        ]
+        for test in tests:
+            self.common(test)
+            assert self.part1() == 5934
+            self.common(test)
+            assert self.part2() == 26984457539
+
+        self.common(input_data)
+        assert self.part1() == 360610
+        self.common(input_data)
+        assert self.part2() == 1631629590423
+
+        return len(tests) + 1
