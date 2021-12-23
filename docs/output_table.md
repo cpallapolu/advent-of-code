8e22d104ee59c61af759a3a7d8c83668ca529dde
Chanakya Pallapolu <chanakya@svtrobotics.com>
2021-12-23 13:13:52 -0500

    added puzzle 15

--- docs/output_table.md
+++ docs/output_table.md
@@ -1,8 +1,8 @@
 <table>
 <thead>
-<tr><th>#  </th><th>Part 1  </th><th>Part 1 Time  </th><th>Part 2       </th><th>Part 2 Time  </th><th>Tests  </th><th>Tests Time  </th></tr>
+<tr><th>#  </th><th>Part 1  </th><th>Part 1 Time  </th><th>Part 2  </th><th>Part 2 Time  </th><th>Tests  </th><th>Tests Time  </th></tr>
 </thead>
 <tbody>
-<tr><td>14 </td><td>2345    </td><td>1.413 ms     </td><td>2432786807053</td><td>4.018 ms     </td><td>2      </td><td>6.579 ms    </td></tr>
+<tr><td>15 </td><td>621     </td><td>43.020 ms    </td><td>2904    </td><td>1341.582 ms  </td><td>2      </td><td>1572.896 ms </td></tr>
 </tbody>
 </table>
--- src/years/2021/15/solution.py
+++ src/years/2021/15/solution.py
@@ -1,27 +1,91 @@
 
+from heapq import heappop, heappush
 
 from aocpuzzle import AoCPuzzle
 
 
 class Puzzle15(AoCPuzzle):
     def common(self, input_data: list[str]) -> None:
-        print('day15 common in day15')
+        self.input_data = input_data
+
+    def build_map(self, times: int) -> None:
+        self.risk_map: dict[tuple[int, int], int] = {
+            (x, y): int(risk)
+            for x, row in enumerate(self.input_data * times)
+            for y, risk in enumerate(row * times)
+        }
+
+        initial_rows, initial_cols = len(self.input_data), len(self.input_data[0])
+
+        self.last_row = ((len(self.input_data)) * times) - 1
+        self.last_col = ((len(self.input_data[0])) * times) - 1
+
+        if times == 5:
+            for new_row in range(self.last_row + 1):
+                for new_col in range(self.last_col + 1):
+                    self.risk_map[(new_row, new_col)] = 1 + (
+                        self.risk_map[(new_row, new_col)]
+                        + (new_row // initial_rows)
+                        + ((new_col // initial_cols) - 1)
+                    ) % 9
+
+    def get_neighbors(self, row: int, col: int) -> list[tuple[int, int]]:
+        neighbors = []
+
+        for x, y in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            new_row, new_col = row + x, col + y
+
+            if (new_row, new_col) in self.risk_map:
+                neighbors.append((new_row, new_col))
+
+        return neighbors
+
+    def path(self) -> int:
+        pq = [(0, (0, 0))]
+        visited = set({(0, 0)})
+
+        while len(pq) > 0:
+            curr_risk, (row, col) = heappop(pq)
+
+            if row == self.last_row and col == self.last_col:
+                return curr_risk
+
+            for neighbor_row, neighbor_col in self.get_neighbors(row, col):
+                if (neighbor_row, neighbor_col) not in visited:
+                    new_risk = curr_risk + self.risk_map[(neighbor_row, neighbor_col)]
+                    heappush(
+                        pq,
+                        (new_risk, (neighbor_row, neighbor_col)),
+                    )
+                    visited.add((neighbor_row, neighbor_col))
+        return 0
 
     def part1(self) -> int:
-        print('day15 part 1 in day15')
-        return 1
+        self.build_map(1)
+        return self.path()
 
     def part2(self) -> int:
-        print('day15 part 2 in day15')
-        return 2
+        self.build_map(5)
+
+        return self.path()
 
     def test_cases(self, input_data: list[str]) -> int:
-        print('day15 test_cases in day15')
         tests: list[dict] = [
             {
-                'input_data': [],
-                'part1': 1,
-                'part2': 2,
+                'input_data': [
+                    '1163751742',
+                    '1381373672',
+                    '2136511328',
+                    '3694931569',
+                    '7463417111',
+                    '1319128137',
+                    '1359912421',
+                    '3125421639',
+                    '1293138521',
+                    '2311944581',
+                ],
+                'part1': 40,
+                'part2': 315,
             },
         ]
         for test in tests:
@@ -29,5 +93,10 @@ class Puzzle15(AoCPuzzle):
             assert self.part1() == test['part1']
             self.common(test['input_data'])
             assert self.part2() == test['part2']
+
+        self.common(input_data)
+        assert self.part1() == 621
+        self.common(input_data)
+        assert self.part2() == 2904
 
         return len(tests) + 1
